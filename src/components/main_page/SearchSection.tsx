a95d94fe0d6e27f228e07a0b1081267ee5deed3a
soonduck <nederileni99heits@gmail.com>
2024-01-11 16:02:21 +0900

    mod: modify search section

--- src/components/main_page/SearchSection.tsx
+++ src/components/main_page/SearchSection.tsx
@@ -3,7 +3,7 @@ import DepartmentDropdown from "./SearchSection/DepartmentDropdown";
 
 const SearchSection = () => {
   return (
-    <div style={{ display: "flex", width: "100%", marginTop: "80px", marginBottom: "100px" }}>
+    <div style={{ marginTop: "80px", marginBottom: "100px" }}>
       <SearchInput />
       <DepartmentDropdown />
     </div>
--- src/components/main_page/SearchSection/SearchInput.tsx
+++ src/components/main_page/SearchSection/SearchInput.tsx
@@ -1,9 +1,25 @@
-import { Input } from "antd";
-
-const { Search } = Input;
+import { Input, Button, Space, ConfigProvider } from "antd";
+import { SearchOutlined } from "@ant-design/icons";
 
 const SearchInput = () => {
-  return <Search size="large" placeholder="상품을 검색해 보세요" style={{ flex: 2, marginRight: "8px" }} />;
+  return (
+    <>
+      <ConfigProvider
+        theme={{
+          components: {
+            Button: {
+              onlyIconSize: "38px",
+            },
+          },
+        }}
+      >
+        <Space.Compact style={{ marginRight: 20 }}>
+          <Input placeholder="상품을 검색해 보세요" style={{ fontSize: "20px", width: "570px", height: "60px" }} />
+          <Button type="primary" icon={<SearchOutlined />} style={{ width: "60px", height: "60px" }}></Button>
+        </Space.Compact>
+      </ConfigProvider>
+    </>
+  );
 };
 
 export default SearchInput;
