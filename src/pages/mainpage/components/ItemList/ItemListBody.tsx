2f2f4053e64393ebe810810c2a810323120bd86c
soonduck <nederileni99heits@gmail.com>
2024-01-13 19:33:16 +0900

    rfc: apply feedback
    1. data 폴더에 있는 DEPARTMENTS 상수 이용
    2. 삼항 연산자 렌더링으로 수정
    3. pages/mainpage/index.tsx 리팩터링

--- src/pages/mainpage/components/ItemList/ItemListBody.tsx
+++ src/pages/mainpage/components/ItemList/ItemListBody.tsx
@@ -13,30 +13,22 @@ interface ItemListBodyProps {
 }
 
 const ItemListBody: React.FC<ItemListBodyProps> = ({ products, maxItemCount }) => {
-  const items = [];
   const itemCount = products.length < maxItemCount ? products.length : maxItemCount;
-  for (let i = 0; i < itemCount; i++) {
-    items.push(
-      <Col xs={24} sm={12} md={8} lg={8} xl={6} key={i}>
-        <ItemButton product={products[i]} />
-      </Col>,
-    );
-  }
 
-  if (itemCount) {
-    return (
-      <Row gutter={{ xs: 8, sm: 16, md: 16, lg: 24 }} css={BodyStyle}>
-        {items}
-      </Row>
-    );
-  } else {
-    return (
-      <div css={NoItemStyle}>
-        <img src={"cat.png"} alt="Image" css={NoItemImageStyle} /> <br />
-        <Text italic>아직 올라온 상품이 없나 봐요.</Text>
-      </div>
-    );
-  }
+  return itemCount ? (
+    <Row gutter={{ xs: 8, sm: 16, md: 16, lg: 24 }} css={BodyStyle}>
+      {products.slice(0, itemCount).map((product, idx) => (
+        <Col xs={24} sm={12} md={8} lg={8} xl={6} key={idx}>
+          <ItemButton product={product} />
+        </Col>
+      ))}
+    </Row>
+  ) : (
+    <div css={NoItemStyle}>
+      <img src={"cat.png"} alt="Image" css={NoItemImageStyle} /> <br />
+      <Text italic>아직 올라온 상품이 없나 봐요.</Text>
+    </div>
+  );
 };
 
 export default ItemListBody;
@@ -52,6 +44,6 @@ const NoItemStyle = css`
 `;
 
 const NoItemImageStyle = css`
-  width: 20%;
+  width: 200px;
   margin-bottom: 10px;
 `;
