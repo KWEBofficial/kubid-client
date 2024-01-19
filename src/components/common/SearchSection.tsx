33fed955e02dd11c503586c8369362676e7a10a3
soonduck <nederileni99heits@gmail.com>
2024-01-19 13:17:26 +0900

    fix: keep last search from mainpage

--- src/components/common/SearchSection.tsx
+++ src/components/common/SearchSection.tsx
@@ -6,7 +6,11 @@ import { useState } from "react";
 import DepartmentDropdown from "./SearchSection/DepartmentDropdown";
 
 const SearchSection = () => {
-  const [selectedDepartmentId, setSelectedDepartmentId] = useState<number | null>(null);
+  const params = new URLSearchParams(window.location.search);
+  const lastSearch = params.get("search") ?? "";
+  const lastDepartmentId = (params.get("departmentId") && Number(params.get("departmentId"))) || null;
+
+  const [selectedDepartmentId, setSelectedDepartmentId] = useState<number | null>(lastDepartmentId);
 
   const handleChangeDepartmentSelect = (value: number | null) => {
     setSelectedDepartmentId(value);
@@ -16,7 +20,7 @@ const SearchSection = () => {
     <div css={SearchSectionStyle}>
       <Row gutter={16}>
         <Col xs={24} md={16} lg={18}>
-          <SearchInput departmentId={selectedDepartmentId ?? undefined} />
+          <SearchInput departmentId={selectedDepartmentId ?? undefined} defaultValue={lastSearch} />
         </Col>
         <Col xs={24} md={8} lg={6}>
           <DepartmentDropdown selectedValue={selectedDepartmentId} changeSelectedValue={handleChangeDepartmentSelect} />
